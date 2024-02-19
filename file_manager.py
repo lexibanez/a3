ffdd915a86fa25da187cc458744e08943553ac5a
Lex Ibanez <laibanez@uci.edu>
2024-02-18 16:16:27 -0800

    made git repo

---
+++ file_manager.py
@@ -0,0 +1,224 @@
+# Lex Ibanez
+# laibanez@uci.edu
+# 70063614
+
+from pathlib import Path
+from Profile import Profile, Post, DsuProfileError
+
+
+def get_last_option(options):
+    if len(options) == 3:
+        return options[2]
+    elif len(options) == 4:
+        return options[3]
+
+
+def list_directory(directory, options):
+    contents = list(directory.iterdir())
+    # sort contents for files
+    files = [x for x in contents if x.is_file()]
+    # sort contents for directories
+    directories = [x for x in contents if x.is_dir()]
+
+    if '-e' in options:
+        suffix = get_last_option(options)
+
+        for file in files:
+            if file.suffix == '.' + suffix:
+                print(file)
+        for directory in directories:
+            if '-r' in options:
+                list_directory(directory, options)
+
+    elif '-s' in options:
+        search_file = get_last_option(options)
+
+        # checks if the search file has a path
+        if Path(directory / search_file).is_file():
+            print(Path(directory / search_file))
+        for directory in directories:
+            if '-r' in options:
+                list_directory(directory, options)
+
+    elif '-f' in options:
+        for file in files:
+            print(file)
+        for directory in directories:
+            if '-r' in options:
+                list_directory(directory, options)
+
+    else:
+        for file in files:
+            print(file)
+        for directory in directories:
+            print(directory)
+            if '-r' in options:
+                list_directory(directory, options)
+
+
+def create_file(directory, options):
+    if not directory.is_dir():
+        print("not a directory")
+        raise Exception("not a directory")
+
+    if "-n" in options:
+        index = options.index('-n')
+        if index + 1 < len(options):
+            file_name = options[index + 1]
+            new_path = directory / f'{file_name}.dsu'
+            new_path.touch()
+        else:
+            print("missing file name")
+            raise Exception("missing file name")
+    else:
+        print("missing -n option")
+        raise Exception("missing -n option")
+
+    return new_path
+
+
+def delete_file(directory):
+    if directory.is_file():
+        if directory.suffix == '.dsu':
+            file_name = directory
+            directory.unlink()
+            print(f'{file_name} DELETED')
+        else:
+            print("ERROR")
+    else:
+        print("ERROR")
+
+
+def read_file(directory):
+    if directory.is_file():
+        if directory.suffix == '.dsu':
+            with open(directory, 'r') as file:
+                content = file.read()
+                if not content:
+                    print("empty file")
+                else:
+                    print(content.strip())
+        else:
+            print("not a dsu file")
+    else:
+        print("not a file")
+
+
+def open_dsu_file(directory):
+    if directory.is_file():
+        if directory.suffix == '.dsu':
+            try:
+                # instantiate a new profile object
+                journal = Profile()
+                # load the profile from the file system into the object
+                journal.load_profile(directory)
+                return journal
+            except DsuProfileError:
+                print("File does not follow Profile format.")
+                raise Exception
+        else:
+            raise Exception("not a .dsu file")
+    else:
+        raise Exception("could not open the file")
+
+
+def edit_dsu_file(journal: Profile, dsu_path: str, command=None, args=None):
+
+    if command.lower() == 'q':
+        return
+
+    elif command.lower() == 'e':
+        if '-usr' in args:
+            username = get_argument_value(args, '-usr')
+            journal.username = username
+            journal.save_profile(dsu_path)
+        if '-pwd' in args:
+            pwd = get_argument_value(args, '-pwd')
+            journal.password = pwd
+            journal.save_profile(dsu_path)
+        if '-bio' in args:
+            bio = get_argument_value(args, '-bio')
+            journal.bio = bio
+            journal.save_profile(dsu_path)
+        if '-addpost' in args:
+            post_content = get_argument_value(args, '-addpost')
+            post = Post(post_content)
+            journal.add_post(post)
+            journal.save_profile(dsu_path)
+        if '-delpost' in args:
+            for arg in args:
+                if arg.isdigit():
+                    index = int(arg)
+                    break
+            journal.del_post(index)
+            journal.save_profile(dsu_path)
+
+        return
+
+    elif command.lower() == 'p':
+        if '-usr' in args:
+            print(f'Your username is {journal.username}\n')
+        if '-pwd' in args:
+            print(f'Your password is {journal.password}\n')
+        if '-bio' in args:
+            print(f'Your bio is {journal.bio}\n')
+        if '-posts' in args:
+            get_all_posts(journal)
+        if '-post' in args:
+            try:
+                id = get_argument_value(args, '-post')
+                print(journal.get_posts()[int(id)]["entry"])
+            except IndexError:
+                print("Index out of bounds. Please try again.")
+        if '-all' in args:
+            print(journal.__str__())
+            get_all_posts(journal)
+
+        return
+
+
+# get the value of the argument after the "-xxx" command including spaces
+def get_argument_value(args, command):
+    if command in args:
+        start_index = args.index(command) + 1
+        end_index = next((i for i,
+                          arg in enumerate(args[start_index:],
+                                           start=start_index)
+                          if arg.startswith('-')), len(args))
+        value = ' '.join(args[start_index:end_index])
+        # get the value of the argument after the
+        # "-xxx" command including spaces
+        return value.strip('\'"')
+    else:
+        return None
+
+
+def get_all_posts(journal):
+    posts = journal.get_posts()
+    i = 0
+    for post in posts:
+        print(f'{i}: {post["entry"]}\nTimestamp: {post["timestamp"]}')
+        i += 1
+
+
+def check_spaces(input):
+    if input.isspace():
+        print('Input cannot be only spaces')
+        return False
+    if input == '':
+        print('Input cannot be empty')
+        return False
+    return True
+
+
+def check_input(input):
+    if input == '':
+        print('Input cannot be empty')
+        return False
+    if ' ' in input:
+        print('Input cannot contain spaces')
+        return False
+    if input.isspace():
+        print('Input cannot be only spaces')
+        return False
+    return True
---
+++ ui.py
@@ -0,0 +1,307 @@
+# Lex Ibanez
+# laibanez@uci.edu
+# 70063614
+
+# testpath
+# C:\Users\lexib\OneDrive\Desktop\ICS32\a2tests
+
+from file_manager import *
+from pathlib import Path
+from Profile import Profile
+
+
+edit_menu_options_list = ['-usr', '-pwd', '-bio', '-addpost', '-delpost']
+print_menu_options_list = ['-usr', '-pwd', '-bio', '-posts', '-post', '-all']
+
+
+def run_ui(option):
+
+    quit_flag = False
+
+    while True:
+        if quit_flag:
+            return
+
+        if option.lower() == 'c':
+            directory = Path(input('Enter the directory where '
+                                   'you would like to create the file: '))
+
+            if directory.is_dir():
+                try:
+                    while True:
+                        print('Enter your username: ', end='')
+                        username = str(input()).strip()
+                        status = check_input(username)
+                        if not status:  # check if the username is valid
+                            continue
+
+                        print('Enter your password: ', end='')
+                        password = str(input()).strip()
+                        # check if the password is valid
+                        status2 = check_input(password)
+                        if not status2:
+                            continue
+                        # if both username and password are valid,
+                        # break out of the loop
+                        break
+                    while True:
+                        bio = str(input('Enter your bio: ')).strip('\'"')
+                        status = check_spaces(bio)
+                        if not status:
+                            continue
+                        break
+                    while True:
+                        file_name = str(input('Enter a name for '
+                                              'the DSU file: ')).strip()
+                        status = check_input(file_name)
+                        if not status:
+                            continue
+                        break
+
+                except Exception:
+                    print("Could not make dsu file, please try again.")
+                    break
+
+                # if the file does not exist, create it
+                if not (directory / (file_name + '.dsu')).exists():
+                    dsu_path = create_file(directory, ['-n', file_name])
+                    journal = Profile(username, password, bio)
+                    journal.save_profile(dsu_path)
+                    print("Dsu file created and currently open")
+
+                # if the file exists, open it
+                elif (directory / (file_name + '.dsu')).exists():
+                    print("File already exists, opening file.")
+                    try:
+                        journal = open_dsu_file(directory/(file_name + '.dsu'))
+                        journal.save_profile(directory / (file_name + '.dsu'))
+                        dsu_path = directory / (file_name + '.dsu')
+                    except Exception:
+                        break
+
+                while True:
+                    print(f"\nCurrently looking at {file_name}.dsu,"
+                          " please enter a journal command, "
+                          "or 'Q' to close journal")
+                    journal_menu_options()
+                    command = input("Enter command: ").lower().strip()
+                    while command not in ["e", "p", "q"]:
+                        print("Invalid command, please try again.")
+                        journal_menu_options()
+                        command = input("Enter command: ").lower().strip()
+
+                    args = []
+
+                    if command == "e":
+                        print(f"\nYou are now editing {file_name}.dsu,"
+                              " please enter an editing command")
+                        edit_menu_options()
+                        option = input("Enter a command: ")
+                        while option not in edit_menu_options_list:
+                            print("Invalid command, please try again.")
+                            edit_menu_options()
+                            option = input("Enter a command: ")
+                        args.append(option)
+                        option_input = handle_edit_options(option, journal)
+                        print("Changes Saved")
+                        args.append(option_input)
+
+                    elif command == "p":
+                        print(f"\nYou are now looking at {file_name}.dsu, "
+                              "please enter an print command")
+                        print_menu_options()
+                        option = input("Enter a command: ")
+                        while option not in print_menu_options_list:
+                            print("Invalid command, please try again.")
+                            edit_menu_options()
+                            option = input("Enter a command: ")
+                        args.append(option)
+                        option_input = handle_print_options(option, journal)
+                        args.append(option_input)
+
+                    if command == 'q':
+                        quit_flag = True
+                        break
+
+                    edit_dsu_file(journal, dsu_path, command, args)
+            else:
+                print("Could not find the specified directory, "
+                      "please try again.")
+
+        elif option.lower() == 'o':
+            while True:
+                try:
+                    directory = Path(input("Please enter the directory of"
+                                           " the file you want to open: "))
+                    journal = open_dsu_file(directory)
+                    journal.save_profile(directory)
+                    break
+                except Exception:
+                    print("Could not find the specified directory, or the "
+                          "path provided is not a .dsu file."
+                          "\nPlease try again.")
+
+            while True:
+                print(f"\nCurrently looking at {directory.name}, please "
+                      "enter a journal command, or 'Q' to close journal")
+                journal_menu_options()
+                command = input("Enter command: ").lower().strip()
+                while command not in ["e", "p", "q"]:
+                    print("Invalid command, please try again.")
+                    journal_menu_options()
+                    command = input("Enter command: ").lower().strip()
+
+                args = []
+
+                if command == "e":
+                    print(f"\nYou are now editing {directory.name}, "
+                          "please enter an editing command")
+                    edit_menu_options()
+                    option = input("Enter a command: ")
+                    while option not in edit_menu_options_list:
+                        print("Invalid command, please try again.")
+                        edit_menu_options()
+                        option = input("Enter a command: ")
+                    args.append(option)
+                    option_input = handle_edit_options(option, journal)
+                    print("Changes Saved")
+                    args.append(option_input)
+
+                elif command == "p":
+                    print(f"\nYou are now looking at {directory.name}, "
+                          "please enter an print command")
+                    print_menu_options()
+                    option = input("Enter a command: ")
+                    while option not in print_menu_options_list:
+                        print("Invalid command, please try again.")
+                        edit_menu_options()
+                        option = input("Enter a command: ")
+                    args.append(option)
+                    option_input = handle_print_options(option, journal)
+                    args.append(option_input)
+
+                if command.lower() == 'q':
+                    quit_flag = True
+                    break
+
+                edit_dsu_file(journal, directory, command, args)
+
+
+def journal_menu_options():
+    print("-------------------------------------")
+    print("E: Edit the current file")
+    print("P: Print contents of the current file")
+    print("Q: Quit editing menu")
+    print("-------------------------------------")
+
+
+def edit_menu_options():
+    print("-------------------------------------")
+    print("-usr: change your username")
+    print("-pwd: change your password")
+    print("-bio: change your bio")
+    print("-addpost: make a post")
+    print("-delpost: delete a post")
+    print("-------------------------------------")
+
+
+def print_menu_options():
+    print("-------------------------------------")
+    print("-usr: display your username")
+    print("-pwd: display your password")
+    print("-bio: display your bio")
+    print("-posts: display all of your posts")
+    print("-post: display a certain post")
+    print("-all: display your whole profile")
+    print("-------------------------------------")
+
+
+def handle_edit_options(option, journal):
+    if option == '-usr':
+        while True:
+            username = input("Enter your new username: ").strip()
+            status = check_input(username)
+            if not status:
+                continue
+            break
+        return username
+    if option == '-pwd':
+        while True:
+            password = input("Enter your new password: ").strip()
+            status = check_input(password)
+            if not status:
+                continue
+            break
+        return password
+    if option == '-bio':
+        while True:
+            bio = input("Enter your new bio: ").strip()
+            if bio == '':
+                print('Bio cannot be empty')
+                continue
+            if bio.isspace():
+                print('Bio cannot be only spaces')
+                continue
+            break
+        return bio
+    if option == '-addpost':
+        while True:
+            post_content = input("Enter the content of your post: ")
+            status = check_spaces(post_content)
+            if not status:
+                continue
+            break
+        return post_content
+    if option == '-delpost':
+        while True:
+            get_all_posts(journal)
+            id = input("Enter the id of the post you would like to delete: ")
+            if not id.isdigit():
+                print("ID must be a number")
+                continue
+            status = check_input(id)
+            if not status:
+                continue
+            break
+        return int(id)
+
+
+def handle_print_options(option, journal):
+    if option == '-usr':
+        return
+    if option == '-pwd':
+        return
+    if option == '-bio':
+        return
+    if option == '-posts':
+        return
+    if option == '-post':
+        while True:
+            get_post_indexes_only(journal)
+            id = input("Enter the id of the post you would like to view: ")
+            status = check_input(id)
+            if not status:
+                continue
+            elif not id.isdigit():
+                print("Post id must be a number")
+                continue
+            break
+        return id
+    if option == '-all':
+        return
+
+
+def get_all_posts(journal):
+    posts = journal.get_posts()
+    i = 0
+    for post in posts:
+        print(f'{i}: {post["entry"]}')
+        i += 1
+
+
+def get_post_indexes_only(journal):
+    posts = journal.get_posts()
+    i = 0
+    for post in posts:
+        print(f'{i}:')
+        i += 1
