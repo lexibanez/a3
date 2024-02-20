cb7a8a2260170e6cc063e27fe195de6b84e1940d
Lex Ibanez <laibanez@uci.edu>
2024-02-20 15:15:42 -0800

    made working client and stores response into datatuple object

--- ds_client.py
+++ ds_client.py
@@ -6,6 +6,12 @@
 # laibanez@uci.edu
 # 70063614
 
+# usr: lexibanez pwd: passwordlol
+
+from ds_protocol import extract_json
+import socket
+import json
+
 def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
   '''
   The send function joins a ds server and sends a message, bio, or both
@@ -18,4 +24,31 @@ def send(server:str, port:int, username:str, password:str, message:str, bio:str=
   :param bio: Optional, a bio for the user.
   '''
   #TODO: return either True or False depending on results of required operation
-  return 
+  data = {
+        "join": {
+            "username": username,
+            "password": password,
+            "token": ''
+        }
+    }
+  
+  json_string = json.dumps(data)
+
+
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((server, port))
+
+    send = client.makefile('w')
+    recv = client.makefile('r')
+
+    send.write(json_string + '\r\n')
+    send.flush()
+
+    resp = recv.readline()
+    response_tuple = extract_json(resp)
+
+  return response_tuple
+
+
+if __name__ == '__main__':
+    send('168.235.86.101', 3021, 'newuser1233424', 'passwordlol', 'hello world')
