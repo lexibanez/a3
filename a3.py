1a52a9c5c6b1f5b1b64afbe6c6d7f78ddf187b22
Lex Ibanez <laibanez@uci.edu>
2024-02-20 16:39:53 -0800

    added functionality so ds_client posts a new message and bio

--- a3.py
+++ a3.py
@@ -7,3 +7,25 @@
 # Lex Ibanez
 # laibanez@uci.edu
 # 70063614
+
+from ui import run_ui
+
+def main():
+    while True:
+        user_input = input("Welcome! Would you like to create or open a "
+                           "DSU file? Enter C to create a file, O to "
+                           "open a file, Q to quit: ")
+        while user_input not in options:
+            user_input = input("Invalid command, please enter C to create "
+                               "a file, O to open a file, Q to quit: ")
+
+        if user_input.lower() == 'q':
+            break
+        else:
+            run_ui(user_input)
+
+
+options = ['admin', 'o', 'c', 'q']
+
+if __name__ == '__main__':
+    main()
--- ds_client.py
+++ ds_client.py
@@ -7,10 +7,12 @@
 # 70063614
 
 # usr: lexibanez pwd: passwordlol
+# token: b4337385-d222-4dfc-bc91-0450e40c0de3
 
 from ds_protocol import extract_json
 import socket
 import json
+import time
 
 def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
   '''
@@ -24,6 +26,67 @@ def send(server:str, port:int, username:str, password:str, message:str, bio:str=
   :param bio: Optional, a bio for the user.
   '''
   #TODO: return either True or False depending on results of required operation
+
+  response_tuple = join_server(server, port, username, password)
+
+  # if username or password is taken, return False
+  if response_tuple.type == 'error':
+    print(response_tuple.message)
+    return False
+  
+  token = response_tuple.token
+  post = {
+    "token": token, 
+    "post": {
+       "entry": message, 
+       "timestamp" : time.time()
+     }
+  }
+  
+  post_string = json.dumps(post)
+
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((server, port))
+
+    send = client.makefile('w')
+    recv = client.makefile('r')
+
+    send.write(post_string + '\r\n')
+    send.flush()
+
+    resp = recv.readline()
+    response_tuple = extract_json(resp)
+
+
+  if bio:
+    bio = {
+    "token": token, 
+    "bio": {
+       "entry": bio, 
+       "timestamp" : time.time()
+     }
+  }
+  
+
+  bio_string = json.dumps(bio)
+
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((server, port))
+
+    send = client.makefile('w')
+    recv = client.makefile('r')
+
+    send.write(bio_string + '\r\n')
+    send.flush()
+
+    resp = recv.readline()
+    response_tuple = extract_json(resp)
+
+  return True
+
+
+
+def join_server(server, port, username, password,):
   data = {
         "join": {
             "username": username,
@@ -31,10 +94,10 @@ def send(server:str, port:int, username:str, password:str, message:str, bio:str=
             "token": ''
         }
     }
-  
+  # create json string with data
   json_string = json.dumps(data)
 
-
+  # start client and join DSP server
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((server, port))
 
@@ -45,10 +108,10 @@ def send(server:str, port:int, username:str, password:str, message:str, bio:str=
     send.flush()
 
     resp = recv.readline()
+    # extract the server response into a named tuple
     response_tuple = extract_json(resp)
 
   return response_tuple
 
-
 if __name__ == '__main__':
-    send('168.235.86.101', 3021, 'newuser1233424', 'passwordlol', 'hello world')
+    send('168.235.86.101', 3021, 'lexibanez', 'passwordlol', ':0', 'testbio')
